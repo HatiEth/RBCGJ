23072ecff0e9bdafbdb00b472311566d10b6cd37
Hati Eth <hati.eth@gmail.com>
2016-04-23 00:48:35 +0200

    kaese & fisch

---
+++ Assets/Scripts/Inspektor/NoIngredientsInspect.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class NoIngredientsInspect : MonoBehaviour, IInspectorInitializer {
+	public void Initialize(IItem item)
+	{
+	}
+}
