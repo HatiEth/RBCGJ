750c6ba65b1ff4fdb6e7094130b9317bb87459bb
Hati Eth <hati.eth@gmail.com>
2016-04-23 01:09:26 +0200

    itemstore event

---
+++ Assets/Scripts/Events/StoreItemEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreItemEvent {
+	public delegate void StoreItemAction(IItem item);
+	public static event StoreItemAction OnStoreItem;
+
+	public static void Send(IItem item)
+	{
+		if (OnStoreItem != null) OnStoreItem(item);
+	}
+}
--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour {
@@ -11,9 +12,17 @@ public class ScoreSystem : MonoBehaviour {
     public Image hp3;
 
 
+	private int Score = 0;
+	private List<IItem> Items = new List<IItem>();
+
 
 	// Use this for initialization
 	void Start () {
+		StoreItemEvent.OnStoreItem += (item) =>
+		{
+			Score += item.Ingredients.Length;
+			Items.Add(item);
+		};
 	}
 	
 	// Update is called once per frame
