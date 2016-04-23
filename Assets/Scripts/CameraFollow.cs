31642cda0699f7c1f8fe5aac7ffad2d3d603ccf9
Hati Eth <hati.eth@gmail.com>
2016-04-23 17:15:34 +0200

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Vector3 Freedoms = Vector3.one;
+	public Transform Followee;
+
+	void LateUpdate () {
+		transform.position = transform.position + Vector3.Scale(Followee.position - transform.position, Freedoms);
+	}
+}
