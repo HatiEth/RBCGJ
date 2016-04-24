949614ca8f63c72be2cea52093d647bb5002f394
Hati Eth <hati.eth@gmail.com>
2016-04-24 13:55:01 +0200

    hg gs layering

--- Assets/Scripts/Behaviours/MotherBehaviour.cs
+++ Assets/Scripts/Behaviours/MotherBehaviour.cs
@@ -30,7 +30,7 @@ public class MotherBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start()
 	{
-		//GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
 		if (quicktimeEvent == null)
 			quicktimeEvent = FindObjectOfType<QuicktimeEvent>();
 
