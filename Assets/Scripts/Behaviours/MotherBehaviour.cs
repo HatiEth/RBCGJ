3362ab56d3c8fd6d8d2ad4ad5323fa5b39ba60fa
Hati Eth <hati.eth@gmail.com>
2016-04-24 13:06:33 +0200

    animationen

--- Assets/Scripts/Behaviours/MotherBehaviour.cs
+++ Assets/Scripts/Behaviours/MotherBehaviour.cs
@@ -1,102 +1,137 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 
 public class MotherBehaviour : MonoBehaviour {
 
-    public IItem holdItem;
-    
-
-    [SerializeField]
-    private ChildBehaviour child;
-
-    public bool inspecting;
-    
-    public QuicktimeEvent quicktimeEvent;
-
-    [SerializeField]
-    private float quickTimeRate;
-
-    // Use this for initialization
-    void Start () {
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
-        quicktimeEvent = FindObjectOfType<QuicktimeEvent>();
-    }
-
-    public void takeFromShelf(IItem item)
-    {
-        holdItem = item;
-        Debug.Log(item.ToString());
-        //Aktiviere Buttons o.Ä.
-    }
-
-    public bool handsFree()
-    {
-        return (holdItem == null);
-    }
-
-    private void setHandsFree()
-    {
-        GrabItemEvent.Send(null);
-        inspecting = false;
-        holdItem = null;
-    }
-
-    public void throwAway()
-    {
-        //AnimationStuff();
-        if (holdItem.Equals(child.holdItem) && !holdItem.HasNut)
-            child.enrage();
+	public IItem holdItem;
 
 
-        setHandsFree();
-    }
-    public void putIntoCart()
-    {
-        StoreItemEvent.Send(holdItem);
-        setHandsFree();
-    }
+	[SerializeField]
+	private ChildBehaviour child;
 
-    public void giveToChild()
-    {
-        child.eat(holdItem);
-        setHandsFree();
-    }
+	public bool inspecting;
 
-    public void inspect()
-    {
+	public QuicktimeEvent quicktimeEvent;
+
+	[SerializeField]
+	private float quickTimeRate;
+
+	Animator anim;
+
+	private int animId_tThrowItem;
+	private int animId_tGiveItem;
+	private int animId_tTakeItemChild;
+	private int animId_tGrabItem;
+	private int animId_tFailTakeItemChild;
+	private int animId_bIsHolding;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0);
+		if (quicktimeEvent == null)
+			quicktimeEvent = FindObjectOfType<QuicktimeEvent>();
+
+		anim = GetComponent<Animator>();
+		animId_tThrowItem = Animator.StringToHash("tThrowItem");
+		animId_tGiveItem = Animator.StringToHash("tGiveItem");
+		animId_tTakeItemChild = Animator.StringToHash("tTakeItemChild");
+		animId_tGrabItem = Animator.StringToHash("tGrabItem");
+		animId_tFailTakeItemChild = Animator.StringToHash("tFailTakeItemChild");
+
+		animId_bIsHolding = Animator.StringToHash("bIsHolding");
+	}
+
+	void LateUpdate()
+	{
+		anim.SetBool(animId_bIsHolding, holdItem != null);
+
+	}
+
+	public void takeFromShelf(IItem item)
+	{
+		holdItem = item;
+		Debug.Log(item.ToString());
+		//Aktiviere Buttons o.Ä.
+	}
+
+	public bool handsFree()
+	{
+		return (holdItem == null);
+	}
+
+	private void setHandsFree()
+	{
+		GrabItemEvent.Send(null);
+		inspecting = false;
+		holdItem = null;
+	}
+
+	public void throwAway()
+	{
+		//AnimationStuff();
+		if (holdItem.Equals(child.holdItem) && !holdItem.HasNut)
+			child.enrage();
+
+		anim.SetTrigger(animId_tThrowItem);
+
+		setHandsFree();
+	}
+	public void putIntoCart()
+	{
+		StoreItemEvent.Send(holdItem);
+		setHandsFree();
+		anim.SetTrigger(animId_tGrabItem);
+	}
+
+	public void giveToChild()
+	{
+		child.eat(holdItem);
+		setHandsFree();
+		anim.SetTrigger(animId_tGiveItem);
+	}
+
+	public void inspect()
+	{
 		inspecting = true;
 		GrabItemEvent.Send(holdItem);
-    }
+		anim.SetTrigger(animId_tTakeItemChild);
+	}
 
-    public void checkChildItem()
-    {
-        if (quicktimeEvent.tryToInspect())
-        { 
-            if(child.holdItem != null)
-            {
-                holdItem = child.TakeItem(false);
-                inspect();
-            }
-        }
-    }
+	public void checkChildItem()
+	{
+		if (quicktimeEvent.tryToInspect())
+		{
+			if (child.holdItem != null)
+			{
+				holdItem = child.TakeItem(false);
+				inspect();
+			}
+		}
+		else
+		{
+			anim.SetTrigger(animId_tFailTakeItemChild);
+		}
+	}
 
-    public void takeFromChild()
-    {
-        float value = Random.Range(0.0f, 3.0f) * quickTimeRate;
-        if (quicktimeEvent.influenceEvent(-value) && child.holdItem != null)
-        {
-            holdItem = child.TakeItem(true);
-            throwAway();
-        }
-    }
+	public void takeFromChild()
+	{
+		float value = Random.Range(0.0f, 3.0f) * quickTimeRate;
+		if (quicktimeEvent.influenceEvent(-value) && child.holdItem != null)
+		{
+			holdItem = child.TakeItem(true);
+			throwAway();
+		}
+	}
 
 
 
-    public void approveKasse()
-    {
-        setHandsFree();
-    }
+	public void approveKasse()
+	{
+		setHandsFree();
+	}
 
 
 
--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -15,37 +15,48 @@ public class InputSystem : MonoBehaviour {
 	{
 		motherBehaviour = GetComponent<MotherBehaviour>();
 		takeControl = GetComponent<TakeControl>();
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-        if(KassenEvent.isRunning())
-        {
-            KassenInput();
-            return;
-        }
+		if (KassenEvent.isRunning())
+		{
+			KassenInput();
+			return;
+		}
 
-        if (Input.GetButtonDown("ActionLeft"))
-            {
-                if (motherBehaviour.handsFree() && motherBehaviour.quicktimeEvent.running)
-                    motherBehaviour.takeFromChild();
-                else if (!motherBehaviour.handsFree())
-                    motherBehaviour.throwAway();
-            }
-            else if (Input.GetButtonDown("ActionUp") && !motherBehaviour.handsFree())
-            {
-                motherBehaviour.giveToChild();
-            }
-            else if (Input.GetButtonDown("ActionDown"))
-            {
-                if (motherBehaviour.handsFree())
-                    motherBehaviour.checkChildItem();
-                else if (motherBehaviour.inspecting)
-                    motherBehaviour.putIntoCart();
-                else
-                    motherBehaviour.inspect();
-        }
+		if (Input.GetButtonDown("ActionLeft"))
+		{
+			if (motherBehaviour.handsFree() && motherBehaviour.quicktimeEvent.running)
+			{
+				motherBehaviour.takeFromChild();
+			}
+			else if (!motherBehaviour.handsFree())
+			{
+				motherBehaviour.throwAway();
+			}
+		}
+		else if (Input.GetButtonDown("ActionUp") && !motherBehaviour.handsFree())
+		{
+			motherBehaviour.giveToChild();
+		}
+		else if (Input.GetButtonDown("ActionDown"))
+		{
+			if (motherBehaviour.handsFree())
+			{
+				motherBehaviour.checkChildItem();
+			}
+			else if (motherBehaviour.inspecting)
+			{
+				motherBehaviour.putIntoCart();
+			}
+			else
+			{
+				motherBehaviour.inspect();
+			}
+		}
 
 		IItem item = null;
 
@@ -64,21 +75,21 @@ public class InputSystem : MonoBehaviour {
 				item = takeControl.take(Enums.TakeType.Unten);
 			}
 		}
-        if(item != null)
-            motherBehaviour.takeFromShelf(item);
+		if (item != null)
+			motherBehaviour.takeFromShelf(item);
 	}
 
-    private void KassenInput()
-    {
-        if (Input.GetButtonDown("ActionLeft"))
-        {
-            motherBehaviour.throwAway();
-            KassenEvent.grabNextItem();
-        }
-        else if (Input.GetButtonDown("ActionDown"))
-        {
-            motherBehaviour.approveKasse();
-            KassenEvent.grabNextItem();
-        }
-    }
+	private void KassenInput()
+	{
+		if (Input.GetButtonDown("ActionLeft"))
+		{
+			motherBehaviour.throwAway();
+			KassenEvent.grabNextItem();
+		}
+		else if (Input.GetButtonDown("ActionDown"))
+		{
+			motherBehaviour.approveKasse();
+			KassenEvent.grabNextItem();
+		}
+	}
 }
